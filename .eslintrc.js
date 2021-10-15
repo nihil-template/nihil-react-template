fd13c0710da6908cefe1e85ce0544f0174da93d5
NIHILncunia <nihil_ncunia@naver.com>
2021-10-16 08:46:24 +0900

    2021 1016 eslint 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -106,6 +106,7 @@ module.exports = {
     'jsx-a11y/anchor-has-content': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
 
     // 타입스크립트 규칙
     '@typescript-eslint/interface-name-prefix': 'off',
